625c518d720b5b847f70bb7e817b0341c3bcfd8c
Cihan Çallı <cihan.callii@gmail.com>
2024-03-28 01:05:17 +0300

    Initial Commit

---
+++ Pinterest Mac App UI/Pinterest_Mac_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinterest_Mac_App_UIApp.swift
+//  Pinterest Mac App UI
+//
+//  Created by Cihan on 28.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Pinterest_Mac_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
